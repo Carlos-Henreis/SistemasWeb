f228d9030f643eff66351959ec95a9532cd9f607
Carlos-Henreis <carlos_henreis@unifei.edu.br>
2017-03-16 00:03:40 -0300

    Implementação dos demais exercicios (2 & 4)

--- Exercicios aula/aula4/Exercicios/Ex2/src/java/cadastro.java
+++ Exercicios aula/aula4/Exercicios/Ex2/src/java/cadastro.java
@@ -17,11 +17,11 @@ public class cadastro extends HttpServlet {
         dataNascimento.setTime(dataNasc); 
         Calendar hoje = Calendar.getInstance();  
         int idade = hoje.get(Calendar.YEAR) - dataNascimento.get(Calendar.YEAR); 
-        if (hoje.get(Calendar.MONTH) < dataNascimento.get(Calendar.MONTH)) {
+        if (hoje.get(Calendar.MONTH) < dataNascimento.get(Calendar.MONTH)+1) {
           idade--;  
         } 
         else { 
-            if (hoje.get(Calendar.MONTH) == dataNascimento.get(Calendar.MONTH) && hoje.get(Calendar.DAY_OF_MONTH) < dataNascimento.get(Calendar.DAY_OF_MONTH)) {
+            if (hoje.get(Calendar.MONTH) == dataNascimento.get(Calendar.MONTH)+1 && hoje.get(Calendar.DAY_OF_MONTH) < dataNascimento.get(Calendar.DAY_OF_MONTH)) {
                 idade--; 
             }
         }
@@ -30,42 +30,44 @@ public class cadastro extends HttpServlet {
     
     String signoAcha (Date dataNasc) {
         Calendar dataNascimento = Calendar.getInstance();  
-        dataNascimento.setTime(dataNasc); 
-        if ((dataNascimento.get(Calendar.DAY_OF_MONTH) >= 21 && dataNascimento.get(Calendar.MONTH) == 3) || (dataNascimento.get(Calendar.DAY_OF_MONTH) < 20 && dataNascimento.get(Calendar.MONTH) == 4)){
+        dataNascimento.setTime(dataNasc);
+        System.out.println(dataNascimento.get(Calendar.DAY_OF_MONTH));
+        System.out.println(dataNascimento.get(Calendar.MONTH)+1);
+        if ((dataNascimento.get(Calendar.DAY_OF_MONTH) >= 21 && dataNascimento.get(Calendar.MONTH)+1 == 3) || (dataNascimento.get(Calendar.DAY_OF_MONTH) < 20 && dataNascimento.get(Calendar.MONTH)+1 == 4)){
             return ("Aries");
         }
-        if ((dataNascimento.get(Calendar.DAY_OF_MONTH) > 20 && dataNascimento.get(Calendar.MONTH) == 4) || (dataNascimento.get(Calendar.DAY_OF_MONTH) <= 21 && dataNascimento.get(Calendar.MONTH) == 5 )){
+        if ((dataNascimento.get(Calendar.DAY_OF_MONTH) > 20 && dataNascimento.get(Calendar.MONTH)+1 == 4) || (dataNascimento.get(Calendar.DAY_OF_MONTH) <= 21 && dataNascimento.get(Calendar.MONTH)+1 == 5 )){
             return ("Touro");
         }else
-        if ((dataNascimento.get(Calendar.DAY_OF_MONTH) >= 22 && dataNascimento.get(Calendar.MONTH) == 5) || (dataNascimento.get(Calendar.DAY_OF_MONTH) <= 21 && dataNascimento.get(Calendar.MONTH) == 6)) {
+        if ((dataNascimento.get(Calendar.DAY_OF_MONTH) >= 22 && dataNascimento.get(Calendar.MONTH)+1 == 5) || (dataNascimento.get(Calendar.DAY_OF_MONTH) <= 21 && dataNascimento.get(Calendar.MONTH)+1 == 6)) {
             return ("Gemios");
         }else
-        if ((dataNascimento.get(Calendar.DAY_OF_MONTH) >= 21 && dataNascimento.get(Calendar.MONTH) == 6) || (dataNascimento.get(Calendar.DAY_OF_MONTH) <= 27 && dataNascimento.get(Calendar.MONTH) == 7)){
+        if ((dataNascimento.get(Calendar.DAY_OF_MONTH) >= 21 && dataNascimento.get(Calendar.MONTH)+1 == 6) || (dataNascimento.get(Calendar.DAY_OF_MONTH) <= 27 && dataNascimento.get(Calendar.MONTH)+1 == 7)){
             return ("Cancer");
         }else
-        if ((dataNascimento.get(Calendar.DAY_OF_MONTH) >= 24 && dataNascimento.get(Calendar.MONTH) == 7) || (dataNascimento.get(Calendar.DAY_OF_MONTH) <= 23 && dataNascimento.get(Calendar.MONTH) == 8)){
+        if ((dataNascimento.get(Calendar.DAY_OF_MONTH) >= 24 && dataNascimento.get(Calendar.MONTH)+1 == 7) || (dataNascimento.get(Calendar.DAY_OF_MONTH) <= 23 && dataNascimento.get(Calendar.MONTH)+1 == 8)){
             return ("Leao");
         }else
-        if ((dataNascimento.get(Calendar.DAY_OF_MONTH) >= 24 && dataNascimento.get(Calendar.MONTH) == 8) || (dataNascimento.get(Calendar.DAY_OF_MONTH) <= 23 && dataNascimento.get(Calendar.MONTH) == 9)){
+        if ((dataNascimento.get(Calendar.DAY_OF_MONTH) >= 24 && dataNascimento.get(Calendar.MONTH)+1 == 8) || (dataNascimento.get(Calendar.DAY_OF_MONTH) <= 23 && dataNascimento.get(Calendar.MONTH)+1 == 9)){
             return ("Virgem");
         }else
-        if ((dataNascimento.get(Calendar.DAY_OF_MONTH) >= 24 && dataNascimento.get(Calendar.MONTH) == 9) || (dataNascimento.get(Calendar.DAY_OF_MONTH) <= 23 && dataNascimento.get(Calendar.MONTH) == 10)){
+        if ((dataNascimento.get(Calendar.DAY_OF_MONTH) >= 24 && dataNascimento.get(Calendar.MONTH)+1 == 9) || (dataNascimento.get(Calendar.DAY_OF_MONTH) <= 23 && dataNascimento.get(Calendar.MONTH)+1 == 10)){
             return ("Libra");
         }else
-        if ((dataNascimento.get(Calendar.DAY_OF_MONTH) >= 24 && dataNascimento.get(Calendar.MONTH) == 10) || (dataNascimento.get(Calendar.DAY_OF_MONTH) <= 22 && dataNascimento.get(Calendar.MONTH) == 11)){
+        if ((dataNascimento.get(Calendar.DAY_OF_MONTH) >= 24 && dataNascimento.get(Calendar.MONTH)+1 == 10) || (dataNascimento.get(Calendar.DAY_OF_MONTH) <= 22 && dataNascimento.get(Calendar.MONTH)+1 == 11)){
             return ("Escorpiao");
         }else
-        if ((dataNascimento.get(Calendar.DAY_OF_MONTH) >= 23 && dataNascimento.get(Calendar.MONTH) == 11) || (dataNascimento.get(Calendar.DAY_OF_MONTH) <= 21 && dataNascimento.get(Calendar.MONTH) == 12)){
+        if ((dataNascimento.get(Calendar.DAY_OF_MONTH) >= 23 && dataNascimento.get(Calendar.MONTH)+1 == 11) || (dataNascimento.get(Calendar.DAY_OF_MONTH) <= 21 && dataNascimento.get(Calendar.MONTH)+1 == 12)){
             return ("Sagitario");
         }else
-        if ((dataNascimento.get(Calendar.DAY_OF_MONTH) >= 22 && dataNascimento.get(Calendar.MONTH) == 11) || (dataNascimento.get(Calendar.DAY_OF_MONTH) <= 20 && dataNascimento.get(Calendar.MONTH) == 1)){
+        if ((dataNascimento.get(Calendar.DAY_OF_MONTH) >= 22 && dataNascimento.get(Calendar.MONTH)+1 == 12) || (dataNascimento.get(Calendar.DAY_OF_MONTH) <= 20 && dataNascimento.get(Calendar.MONTH)+1 == 1)){
             return ("Capricornio");
         }else
-        if ((dataNascimento.get(Calendar.DAY_OF_MONTH) >= 21 && dataNascimento.get(Calendar.MONTH) == 1) || (dataNascimento.get(Calendar.DAY_OF_MONTH) <= 19 && dataNascimento.get(Calendar.MONTH) == 2)){
+        if ((dataNascimento.get(Calendar.DAY_OF_MONTH) >= 21 && dataNascimento.get(Calendar.MONTH)+1 == 1) || (dataNascimento.get(Calendar.DAY_OF_MONTH) <= 19 && dataNascimento.get(Calendar.MONTH)+1 == 2)){
             return ("Aquario");
         }else
-        if ((dataNascimento.get(Calendar.DAY_OF_MONTH) >= 20 && dataNascimento.get(Calendar.MONTH) == 2) || (dataNascimento.get(Calendar.DAY_OF_MONTH) <= 20 && dataNascimento.get(Calendar.MONTH) == 3)){
-            return ("Picis");
+        if ((dataNascimento.get(Calendar.DAY_OF_MONTH) >= 20 && dataNascimento.get(Calendar.MONTH)+1 == 2) || (dataNascimento.get(Calendar.DAY_OF_MONTH) <= 20 && dataNascimento.get(Calendar.MONTH)+1 == 3)){
+            return ("Peixe");
         }
         else {
             return ("Erro");
